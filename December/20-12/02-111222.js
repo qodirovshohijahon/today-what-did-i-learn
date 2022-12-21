dc853bae904a63935aa2a9336d74b95ca6a9d14e
qodirovshohijahon <tasaddux@gmail.com>
2022-12-21 11:45:50 +0500

    [fix:] comparison block and object storage

---
+++ December/20-12/02-111222.js
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------
+    Sana: 17:00 21-12-22
+
+    Muallif: Ali Valiyev (Front end 09)
+
+    Masala: Ayirmani toping 
+----------------------------------------------------------*/
+
+function substraction(num1, num2){
+    return num1 / num2
+}
+
+//console.log(substraction(110, 21))
+
+//console.log(substraction(1000, 122))
+
+//console.log(substraction(10, 98))
+
+substraction(110, 21)
+
+substraction(1000, 122)
+
+substraction(10, 98)
+
